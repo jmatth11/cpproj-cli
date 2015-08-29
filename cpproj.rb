eb3576861dc7d7b026183f545a94f330373ec52b
Joshua Matthews <artroomemergency@hotmail.com>
2015-08-29 17:53:28 -0500

    Added support for generating classes for you and automatically adding them to Makefile and added build and run functionality

--- cpproj.rb
+++ cpproj.rb
@@ -8,6 +8,8 @@ Be able to add fields when creating class files which will also create getters a
 
 $libs = Array.new
 $files = Array.new
+$obj_files = Array.new
+$proj_name = ""
 $std_cpp = "-std=c++11"
 $comp = "clang++"
 
@@ -25,7 +27,12 @@ def initial_argument arg
 		else
 			puts "Must supply a class name as argument."
 		end
-
+	when "build"
+		`make clean`
+		`make`
+		puts "Build complete!"
+	when "run"
+		run_project
 	when "help"
 		show_help
 	else
@@ -44,9 +51,11 @@ def show_help
 	puts "			Creates project, build, and src folders. Inside project "
 	puts "			folder a Makefile. Inside src folder a Main.cpp file."
 	puts ""
-	puts "	create_class	Argument passed: name of class. Creates a .cpp and .h file"
+	puts "	create_class	Argument passed: name of class. Creates a .cpp and .hpp file"
 	puts "			with defualt constructor and destructor setup for you. Adds"
 	puts "			file to Makefile as well."
+	puts ""
+	puts "	build		"
 	puts ""
 	puts "	help		Shows this menu."
 	puts ""
@@ -57,8 +66,9 @@ def create_project name
 	puts "creating project..."
 	if !File.directory?("./"+name) then
 		Dir.mkdir name, 0700
-		$files<<"Main"
-		create_makefile name
+		$files<<"./src/Main"
+		$obj_files << "./build/Main"
+		create_makefile "./build/#{name}"
 		puts "#{name}/src"
 		Dir.mkdir "#{name}/src", 0700
 		puts "#{name}/build"
@@ -72,53 +82,139 @@ def create_project name
 end
 
 def create_makefile name
-	str = "CC=clang++
-	CFLAGS=-Wall -c
-	TARGET=#{name}
-	OBJECTS="
+str = "CC=clang++
+CFLAGS=-Wall -c
+TARGET=#{name}
+OBJECTS="
+tmp = ""
+$files.each do |f|
+	tmp = tmp + "#{f}.o "
+end
+str = str + tmp + "\n"
+str = str + "COBJS="
+tmp = ""
+$obj_files.each do |f|
+	tmp = tmp + "#{f}.o "
+end
+str = str + tmp + "\n"
+if $libs.count > 0 then
+	str = str + "\nLIBS="
 	tmp = ""
-	$files.each do |f|
-		tmp = tmp + f + ".o "
+	$libs.each do |l|
+		tmp = tmp + l + " "
 	end
 	str = str + tmp + "\n"
-	if $libs.count > 0 then
-		str = str + "\nLIBS="
-		tmp = ""
-		$libs.each do |l|
-			tmp = tmp + l + " "
-		end
-		str = str + tmp + "\n"
-	end
-	str = str + "all: $(TARGET)
-	$(TARGET): $(OBJECTS)
-		$(CC) #{$std_cpp} $(OBJECTS) -o $(TARGET)
-	"
-	$files.each do |f|
-	str = str + "#{f}.o: #{f}.cpp
-		$(CC) $(CFLAGS) #{$std_cpp} #{f}.cpp\n"
-	end
-	str = str + "clean: 
-		rm -Rf *.o $(TARGET)
-	"
-	
-	File.write "#{name}/Makefile", str
-	puts "#{name}/Makefile"
+end
+str = str + "all: $(TARGET)
+$(TARGET): $(OBJECTS)
+	$(CC) #{$std_cpp} $(COBJS) -o $(TARGET)
+"
+$files.each do |f|
+str = str + "#{f}.o: #{f}.cpp
+	$(CC) $(CFLAGS) #{$std_cpp} #{f}.cpp
+	mv #{f[/^.+\/(.+)$/,1]}.o ./build/#{f[/^.+\/(.+)$/,1]}.o\n"
+end
+str = str + "clean: 
+	rm -Rf ./build/*.o $(TARGET)
+"
+if File.exist? "Makefile" then
+	File.write "Makefile", str
+	puts "Updated Makefile"
+else
+	File.write "#{name[/\.\/build\/(.+)/,1]}/Makefile", str
+	puts "#{name[/\.\/build\/(.+)/,1]}/Makefile"
+end
 end
 
 
 def create_class name
 
 	if File.exist?("Makefile") && File.directory?("./src") then
-
+		if File.exist?("./src/#{name}.cpp") || File.exist?("./src/#{name}.hpp") then
+			puts "There is already #{name}.cpp or #{name}.hpp created."
+		else
+			puts "./src/#{name}.cpp"
+			File.write "./src/#{name}.cpp", cpp_setup(name)
+			puts "./src/#{name}.hpp"
+			File.write "./src/#{name}.hpp", hpp_setup(name)
+			get_objects_from_make
+			$files << "./src/#{name}"
+			$obj_files << "./build/#{name}"
+			create_makefile $proj_name
+		end
 	else
-		puts "src directory is not in immediate directory. Move to top directory in project or create a project if you have not yet."
+		puts "src directory and Makefile are not in immediate directory. Move to top directory in project or create a project if you have not yet."
 		show_help
 	end
 
 end
 
+def get_objects_from_make
+	open("Makefile").each_line do |line|
+		$proj_name = line[/TARGET=(.+)/,1] if line =~ /TARGET=.+/
+		if line =~ /OBJECTS=.+/ then
+			tmp = line[/OBJECTS=(.+)/,1].split ".o "
+			$files = tmp
+		end
+		if line =~ /COBJS=.+/ then
+			tmp = line[/COBJS=(.+)/,1].split ".o "
+			$obj_files = tmp
+			break
+		end
+	end
+end
+
+def run_project
+	if File.exist?("Makefile") && File.directory?("./build") then
+		open("Makefile").each_line do |line|
+			if line =~ /TARGET=.+/ then
+				ex = line[/TARGET=(.+)/,1]
+				if File.exist?("#{ex}") then
+					exec("#{ex}")
+				else
+					`make`
+					exec("#{ex}")
+				end
+			end
+		end
+		puts "There is no TARGET set in Makefile."
+	else
+		puts "build directory and Makefile are not in immediate directory. Move to top directory in project or create a project if you have not yet."
+	end
+end
+
+def hpp_setup name
+
+return "#ifndef #{name.upcase}_HPP
+#define #{name.upcase}_HPP
+
+class #{name} {
+
+public:
+	#{name}();
+	~#{name}();
+
+};
+
+#endif
+"
+end
+
+def cpp_setup name
+return "#include \"#{name}.hpp\"
+
+#{name}::#{name}() {
+
+}
+
+#{name}::~#{name}() {
+
+}
+"
+end
+
 if ARGV.count > 0 then
 	initial_argument ARGV[0]
 else
-show_help
+	show_help
 end

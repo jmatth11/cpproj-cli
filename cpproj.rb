6b505b8218ca9f4e028f496b205ea5ca02fe3a98
Joshua Matthews <artroomemergency@hotmail.com>
2015-09-04 00:17:44 -0500

    Added functionality to remove classes that have been created and update Makefile accordingly

--- cpproj.rb
+++ cpproj.rb
@@ -6,8 +6,8 @@ Be able to add fields when creating class files which will also create getters a
 
 =end
 
-# array for libraries added (future ability)
-$libs = Array.new
+# string for libraries added (future ability)
+$libs = ""
 # array for cpp files in project
 $files = Array.new
 # array for object files in project
@@ -18,6 +18,8 @@ $proj_name = ""
 $std_cpp = "-std=c++11"
 # compiler used
 $comp = "clang++"
+# flags used for compilation
+$cflags = "-Wall -c"
 
 # function to handle the initial arguments passed
 # @param arg
@@ -30,8 +32,17 @@ def initial_argument arg
 			puts "Must supply a project name as argument."
 		end
 	when "create_class"
-		if ARGV.count > 1 then
+		if ARGV.count > 2 then
+			# implement function to add fields to class when creating it
+			create_class ARGV[1], ARGV.drop(2)
+		elsif ARGV.count > 1 
 			create_class ARGV[1]
+		else
+			puts "Must supply a class name as argument."
+		end
+	when "remove_class"
+		if ARGV.count > 1 then
+			remove_class ARGV[1]
 		else
 			puts "Must supply a class name as argument."
 		end
@@ -63,6 +74,10 @@ def show_help
 	puts "	create_class	Argument passed: name of class. Creates a .cpp and .hpp file"
 	puts "			with defualt constructor and destructor setup for you. Adds"
 	puts "			file to Makefile as well."
+	puts ""
+	puts "	remove_class	Argument passed: name of class. Deletes .cpp and .hpp files"
+	puts "			of class name passed. Removes references of class in Makefile"
+	puts "			as well."
 	puts ""
 	puts "	build		No argument. Command will compile project and place object "
 	puts "			files and executable in build directory. This command cleans"
@@ -100,8 +115,8 @@ end
 # Function that is used for creating and updating the Makefile for project
 # @param name
 def create_makefile name
-str = "CC=clang++
-CFLAGS=-Wall -c
+str = "CC=#{$comp}
+CFLAGS=#{$cflags}
 TARGET=#{name}
 OBJECTS="
 tmp = ""
@@ -114,18 +129,10 @@ tmp = ""
 $obj_files.each do |f|
 	tmp = tmp + "#{f}.o "
 end
-str = str + tmp + "\n"
-if $libs.count > 0 then
-	str = str + "\nLIBS="
-	tmp = ""
-	$libs.each do |l|
-		tmp = tmp + l + " "
-	end
-	str = str + tmp + "\n"
-end
+str = str + tmp + "\nLIBS=#{$libs}\n"
 str = str + "all: $(TARGET)
 $(TARGET): $(OBJECTS)
-	$(CC) #{$std_cpp} $(COBJS) -o $(TARGET)
+	$(CC) #{$std_cpp} $(COBJS) $(LIBS) -o $(TARGET)
 "
 $files.each do |f|
 str = str + "#{f}.o: #{f}.cpp
@@ -147,20 +154,45 @@ end
 # Function that takes care of creating new classes for project.
 # Will update Makefile also when ran
 # @param name
-def create_class name
+def create_class name, arr=nil
 
 	if File.exist?("Makefile") && File.directory?("./src") then
 		if File.exist?("./src/#{name}.cpp") || File.exist?("./src/#{name}.hpp") then
 			puts "There is already #{name}.cpp or #{name}.hpp created."
 		else
-			puts "./src/#{name}.cpp"
-			File.write "./src/#{name}.cpp", cpp_setup(name)
-			puts "./src/#{name}.hpp"
-			File.write "./src/#{name}.hpp", hpp_setup(name)
+			if arr == nil then
+				puts "./src/#{name}.cpp"
+				File.write "./src/#{name}.cpp", cpp_setup(name)
+				puts "./src/#{name}.hpp"
+				File.write "./src/#{name}.hpp", hpp_setup(name)
+				get_objects_from_make
+				$files << "./src/#{name}"
+				$obj_files << "./build/#{name}"
+				create_makefile $proj_name
+			else
+				
+			end
+		end
+	else
+		puts "src directory and Makefile are not in immediate directory. Move to top directory in project or create a project if you have not yet."
+		show_help
+	end
+
+end
+
+def remove_class name
+	if File.exist?("Makefile") && File.directory?("./src") then
+		if File.exist?("./src/#{name}.cpp") || File.exist?("./src/#{name}.hpp") then
+			puts "removing ./src/#{name}.cpp"
+			File.delete "./src/#{name}.cpp"
+			puts "removing ./src/#{name}.hpp"
+			File.delete "./src/#{name}.hpp"
 			get_objects_from_make
-			$files << "./src/#{name}"
-			$obj_files << "./build/#{name}"
+			$files = $files.delete_if {|x| x == "./src/#{name}"}
+			$obj_files = $obj_files.delete_if {|x| x == "./build/#{name}"}
 			create_makefile $proj_name
+		else
+			puts "The files #{name}.cpp or #{name}.hpp do not exist."
 		end
 	else
 		puts "src directory and Makefile are not in immediate directory. Move to top directory in project or create a project if you have not yet."
@@ -173,13 +205,19 @@ end
 def get_objects_from_make
 	open("Makefile").each_line do |line|
 		$proj_name = line[/TARGET=(.+)/,1] if line =~ /TARGET=.+/
-		if line =~ /OBJECTS=.+/ then
+		if line =~ /CC=.+/ then
+			$comp = line[/CC=(.+)/,1]
+		elsif line =~ /CFLAGS=.+/
+			$cflags = line[/CFLAGS=(.+)/,1]
+		elsif line =~ /OBJECTS=.+/
 			tmp = line[/OBJECTS=(.+)/,1].split ".o "
 			$files = tmp
-		end
-		if line =~ /COBJS=.+/ then
+		elsif line =~ /COBJS=.+/
 			tmp = line[/COBJS=(.+)/,1].split ".o "
 			$obj_files = tmp
+		elsif line =~ /LIBS=.+/
+			$libs = line[/LIBS=(.+)/,1]
+		elsif line =~ /all:.+/
 			break
 		end
 	end
@@ -191,12 +229,9 @@ def run_project
 		open("Makefile").each_line do |line|
 			if line =~ /TARGET=.+/ then
 				ex = line[/TARGET=(.+)/,1]
-				if File.exist?("#{ex}") then
-					exec("#{ex}")
-				else
-					`make`
-					exec("#{ex}")
-				end
+				`make clean`
+				`make`
+				exec("#{ex}")
 			end
 		end
 		puts "There is no TARGET set in Makefile."
